b8655ec89fcf7fcee69e9625b96ef9cb06b7d2d1
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-10-11 05:07:18 +0200

    few vite modification from previous website

---
+++ vite.config.ts
@@ -0,0 +1,75 @@
+import { defineConfig } from 'vite';
+
+import solidPlugin from 'vite-plugin-solid';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import viteCSP from "vite-plugin-csp";
+
+import dotenv from "dotenv";
+
+import autoprefixer from "autoprefixer";
+
+const env = dotenv.config({path: process.cwd() + "/.env"});
+
+// const oneTimeRenderCode = Util.generateString(12);
+
+export default defineConfig({
+  plugins: [
+    solidPlugin(),
+
+    createHtmlPlugin({
+      minify: false,
+      template: "/index.html"
+    }),
+
+    viteCSP({
+      policy: {
+        "img-src": ['self'],
+        "default-src": "self",
+        "script-src": "self",
+        "frame-src": ["none"],
+        "connect-src": ["self"],
+        "font-src": [
+          "self",
+          // "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/",
+          "https://fonts.gstatic.com/s/",
+        ],
+        "style-src": [
+          "self", "unsafe-inline",
+          // "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/",
+          "https://fonts.googleapis.com/"
+        ]
+      }
+    })
+  ],
+  
+  build: {
+    cssCodeSplit: false,
+    minify: true,
+    target: 'esnext',
+    // rollupOptions: {
+    //   output: {
+    //     assetFileNames: () => {
+    //       return `s/${oneTimeRenderCode}/[hash][extname]`;
+    //     },
+
+    //     chunkFileNames: () => {
+    //       return `s/${oneTimeRenderCode}/[hash].js`;
+    //     },
+
+    //     entryFileNames: () => {
+    //       return `s/${oneTimeRenderCode}/[hash].js`;
+    //     }
+    //   }
+    // }
+  },
+
+  preview: {
+    port: Number(env.parsed?.["PORT"] || 5173)
+  },
+
+  css: {
+    postcss: {
+      plugins: process.env.npm_lifecycle_event == "dev" ? [] : [ autoprefixer({ supports: true }) ],
+    }
+  }
+});
