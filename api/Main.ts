426b7065a512e9fdeffce46bade589f704ee7d8c
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-12-16 12:49:13 +0100

    added api server

---
+++ api/Main.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import Express from 'express';
+
+const app = Express();
+
+// basic security
+import helmet from "helmet";
+app.use(helmet());
+app.use(Express.json({strict: true, type: "application/json"}));
+
+// cors
+import CORS from "cors";
+app.use(CORS({
+  origin: ["https://prodigal.13373333.one", "https://13373333.one"]
+}))
+
+// OK
+app.get("/", (_, res) => {
+  return res.status(200).send("my pussi pink just like salami");
+});
+
+// file storage
+import FileStorage from "./routes/FileStorage";
+app.use("/file", FileStorage);
+
+const PORT = +process.env.API_PORT!;
+app.listen(PORT, () => {
+  console.log("Media: Connected with port", PORT);
+});
