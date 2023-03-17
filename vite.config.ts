052c3934e99462a682570be233295c7b2e2797ce
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-03-17 18:40:15 +0100

    put CSP on header instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
-// import swc from 'unplugin-swc';
 import solidPlugin from 'vite-plugin-solid';
 import { createHtmlPlugin } from 'vite-plugin-html';
-import viteCSP from "vite-plugin-csp";
 import dotenv from "dotenv";
 import autoprefixer from "autoprefixer";
 import { generateString } from './src/Util';
@@ -11,7 +9,27 @@ const env = dotenv.config({path: process.cwd() + "/.env"});
 
 const oneTimeRenderCode = generateString(12);
 
-// @ts-expect-error
+// content security policy
+const precollectedCSP = {
+  "img-src": ['self', "https://13373333.one/favicon.webp"],
+  "default-src": ["self"],
+  "script-src": ["self"],
+  "frame-src": ["none"],
+  "connect-src": ["self", "https://13373333.one/3333/file/"],
+  "font-src": [
+    "self",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/",
+    "https://fonts.gstatic.com/s/",
+  ],
+  "style-src": [
+    "self", "unsafe-inline",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/",
+    "https://fonts.googleapis.com/"
+  ]
+};
+
+const combinedCSP = Object.entries(precollectedCSP).map(([directive, sources]) => `${directive} ${sources.join(' ')};`).join(' ');
+
 export default defineConfig({
   plugins: [
     solidPlugin(),
@@ -23,26 +41,6 @@ export default defineConfig({
         data: {
           "cssPreviewDevOnly": process.env.npm_lifecycle_event == "dev" ? `<link href="./src/css/Preview.css" rel="preload stylesheet" as="style"/>` : "",
         }
-      }
-    }),
-
-    viteCSP({
-      policy: {
-        "img-src": ['self', "https://13373333.one/favicon.webp"],
-        "default-src": "self",
-        "script-src": "self",
-        "frame-src": ["none"],
-        "connect-src": ["self", "https://13373333.one/3333/file/"],
-        "font-src": [
-          "self",
-          "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/",
-          "https://fonts.gstatic.com/s/",
-        ],
-        "style-src": [
-          "self", "unsafe-inline",
-          "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/",
-          "https://fonts.googleapis.com/"
-        ]
       }
     })
   ],
@@ -69,11 +67,17 @@ export default defineConfig({
   },
 
   preview: {
-    port: Number(env.parsed?.["PORT"] || 5173)
+    port: Number(env.parsed?.["PORT"] || 5173),
+    headers: {
+      "content-security-policy": combinedCSP
+    }
   },
 
   server: {
-    port: Number(env.parsed?.["DEVPORT"] || 5172)
+    port: Number(env.parsed?.["DEVPORT"] || 5172),
+    headers: {
+      "content-security-policy": combinedCSP
+    }
   },
 
   css: {
