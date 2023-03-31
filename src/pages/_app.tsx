9b878a6b475cd2009df7ce3d80382a3943a14360
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-03-31 09:27:50 +0200

    remove unnecessary _app.tsx

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app';
-import '@/css/Reset.css';
-import '@/css/Base.css';
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (<> <Component {...pageProps} /> </>);
-}
