f1480ce48b8e5734154c744f60297b68729139e9
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-04-29 05:11:27 +0200

    move projects list to a different file

---
+++ src/pages/Main/components/Project/Project.List.tsx
@@ -0,0 +1,99 @@
+// icon
+import { siAbletonlive, siNextdotjs, siTypescript, siSolid, siJavascript, siDiscord, siMongodb, siRedis, siObsstudio, siYoutube, siFfmpeg, siTensorflow, siExpress, siPostgresql, siSoundcloud, siNodedotjs } from "simple-icons/icons";
+
+// images
+import _projImg_ire from "../../../assets/proj/ire.webp";
+import _projImg_self from "../../../assets/proj/1337.webp";
+import _projImg_cdev from "../../../assets/proj/cdev.webp";
+import _projImg_blobproj from "../../../assets/proj/blobproj.webp";
+import _projImg_gmdibot from "../../../assets/proj/gmdibot.webp";
+import _projImg_antinsfw from "../../../assets/proj/antinsfw.webp";
+import _projImg_cupcakke from "../../../assets/proj/cupcakke.webp";
+import _projImg_catbox from "../../../assets/proj/catbox.webp";
+import _projImg_eom from "../../../assets/proj/eom.webp";
+import _projImg_dbm from "../../../assets/proj/dbm.webp";
+import _projImg_phash from "../../../assets/proj/phash.webp";
+
+export default [
+  {
+    title: "13373333.one",
+    image: _projImg_self,
+    url: "https://github.com/ray-1337/solid-13373333-v2",
+    description: "An open-source repository of this website.",
+    tools: [siTypescript, siExpress]
+  },
+  {
+    title: "Perceptual Hash",
+    image: _projImg_phash,
+    url: "https://npmjs.com/package/perceptual-hash",
+    description: "A simple way to generate content-based image hashes on Node.js, also used on Anti-NSFW.",
+    tools: [siTypescript, siNodedotjs]
+  },
+  {
+    title: "CupcakKe",
+    image: _projImg_cupcakke,
+    url: "https://www.youtube.com/playlist?list=PLGd05QsjGyxUrnRbKBGB9touvcyj51MJW",
+    description: "A parodies of CupcakKe, remixed by me.",
+    tools: [siYoutube, siAbletonlive, siSoundcloud]
+  },
+  {
+    title: "Catbox",
+    image: _projImg_catbox,
+    url: "https://13373333.one/catbox",
+    description: "A personal/private file storage. Powered by BunnyCDN.",
+    tools: [siTypescript, siSolid]
+  },
+  {
+    title: "Community Development",
+    image: _projImg_cdev,
+    url: "https://cdev.shop",
+    description: "My first remotely startup project to advance limitations around Discord and FiveM communities.",
+    tools: [siTypescript, siRedis, siNextdotjs, siMongodb, siDiscord, siPostgresql],
+    resigned: 0
+  },
+  {
+    title: "Erase Our Memories",
+    image: _projImg_eom,
+    url: "https://github.com/ray-1337/erase-our-memories/",
+    description: "A side-project/script to bulk-delete your messages from your exes contact.",
+    tools: [siDiscord, siJavascript]
+  },
+  {
+    title: "IRE (03.12.2021)",
+    image: _projImg_ire,
+    url: "https://soundcloud.com/1337-3333/sets/ire",
+    description: "A way to distract me from self-harm, but it didn't help.",
+    tools: [siSoundcloud],
+    resigned: 1
+  },
+  {
+    title: "Blob Project",
+    image: _projImg_blobproj,
+    url: "https://blob-project.com",
+    description: "Creating a YouTube programming content with no retakes, and no fillers.",
+    tools: [siObsstudio, siDiscord, siYoutube]
+  },
+  {
+    title: "Anti-NSFW",
+    image: _projImg_antinsfw,
+    url: "https://docs.cdev.shop/anti-nsfw/grand-mirage",
+    description: "A Discord bot that can detect NSFW content through machine learning.",
+    tools: [siTypescript, siRedis, siMongodb, siDiscord, siTensorflow, siExpress]
+  },
+  {
+    title: "GMDI Discord Bot",
+    image: _projImg_gmdibot,
+    url: "https://github.com/ray-1337/gmdi-private-bot/",
+    description: "A Discord bot that is made exclusively for Geometry Dash Indonesia.",
+    tools: [siTypescript, siDiscord, siExpress],
+    resigned: 0
+  },
+  {
+    title: "Discord & YouTube bot music",
+    image: _projImg_dbm,
+    url: "https://github.com/ray-1337/discord-music-bot/",
+    description: "Newest project to solve concerns around Discord music bot era by giving it open source.",
+    tools: [siTypescript, siDiscord, siYoutube, siFfmpeg],
+    resigned: 0
+  }
+];
