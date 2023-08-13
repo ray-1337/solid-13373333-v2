0848c46f1df420b0a8565ac907cf50794eac4589
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-08-14 00:47:15 +0200

    revamp Tools section

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -54,64 +54,63 @@ import {
   siWebassembly,
 } from "simple-icons/icons";
 
-import { Component, For, createSignal, createEffect, Show } from "solid-js";
-import { sleep } from "../../../Util";
+import { useState, useEffect } from "react";
+import { sleep } from "../Util";
 
 import style from "./Tools.module.css";
 import headingStyle from "../../../css/Main.module.css";
 
-const Personal: Component<{ active?: boolean }> = (props: { active?: boolean }) => {
+export default function(props: { active?: boolean }) {
   const derivedProps = () => props.active;
-  const [toggle, setToggle] = createSignal<boolean>(false), [deferToggle, setDeferToggle] = createSignal<boolean>(false);
-  createEffect(() => {
+
+  const [toggle, setToggle] = useState<boolean>(false);
+  const [deferToggle, setDeferToggle] = useState<boolean>(false);
+
+  useEffect(() => {
     derivedProps() ? setToggle(true) : sleep(750).then(() => setToggle(false));
-    toggle() ? setTimeout(() => setDeferToggle(true), 5) : setDeferToggle(false);
+    toggle ? setTimeout(() => setDeferToggle(true), 5) : setDeferToggle(false);
   });
 
   return (
-    <Show when={toggle()}>
-      <div data-itchi={String(Boolean(deferToggle()))}>
-        {/* heading */}
-        <div class={headingStyle.heading_confront} style={{ background: "linear-gradient(90deg, rgba(95,117,223,1) 0%, rgba(84,155,235,1) 100%)" }}>
-          <div class={headingStyle.title}> <h1>Tools</h1> </div>
-          <div class={headingStyle.sub}> <p>This is a list of my daily basis tools and other things that I choose to learn for.</p> </div>
-        </div>
-
-        <For each={Object.keys(IconList())}>{
-          (type) => {
-            const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-            const content = IconList()[type];
-
-            return (
-              <div class={style.tools} style={{background: "#" + content.bgColor}}>
-                <div class={style.info}>
-                  <h3>{capitalize(type)}</h3>
-                  <p>{content.description}</p>
-                </div>
-
-                <div class={style.list}>
-                  <For each={content.purified}>{
-                    (state) => {
-                      return (
-                        <span class={style.brand} onclick={() => window.open(state.url, "_blank")}>
-                          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path fill={`#${state.hex != "000000" ? state.hex : "FFFFFF"}`} d={state.path} />
-                          </svg>
-                        </span>
-                      );
-                    }
-                  }</For>
-                </div>
-              </div>
-            );
-          }}
-        </For>
+    <div data-itchi={String(deferToggle)}>
+      {/* heading */}
+      <div className={headingStyle.heading_confront} style={{ background: "linear-gradient(90deg, rgba(95,117,223,1) 0%, rgba(84,155,235,1) 100%)" }}>
+        <div className={headingStyle.title}> <h1>Tools</h1> </div>
+        <div className={headingStyle.sub}> <p>This is a list of my daily basis tools and other things that I choose to learn for.</p> </div>
       </div>
-    </Show>
+
+      {
+        Object.keys(IconList()).map((type, iconIndex) => {
+          const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+          const content = IconList()[type];
+
+          return (
+            <div key={iconIndex} className={style.tools} style={{ background: "#" + content.bgColor }}>
+              <div className={style.info}>
+                <h3>{capitalize(type)}</h3>
+                <p>{content.description}</p>
+              </div>
+
+              <div className={style.list}>
+                {
+                  content.purified.map((state, purifiedIndex) => {
+                    return (
+                      <span className={style.brand} key={purifiedIndex} onClick={() => window.open(state.url, "_blank")}>
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path fill={`#${state.hex != "000000" ? state.hex : "FFFFFF"}`} d={state.path} />
+                        </svg>
+                      </span>
+                    );
+                  })
+                }
+              </div>
+            </div>
+          );
+        })
+      }
+    </div>
   )
 };
-
-export default Personal;
 
 type GeneralListType = { title: string, path: string, hex: string, url: string };
 
