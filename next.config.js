f08d45e02d1ad6f398103af693dc017cd205507b
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-08-13 23:56:44 +0200

    add next-safe, copy from vite.config.ts

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const nextSafe = require("next-safe");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
@@ -5,6 +7,36 @@ const nextConfig = {
     unoptimized: true
   },
 
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: nextSafe({
+          isDev: process.env.NODE_ENV !== 'production',
+
+          contentSecurityPolicy: {
+            "img-src": ["'self'"],
+            "default-src": ["'self'"],
+            "script-src": ["'self'"],
+            "frame-src": ["'self'", "https://www.youtube-nocookie.com/embed/"],
+            "connect-src": ["'self'"],
+            "font-src": [
+              "'self'",
+              "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/",
+              "https://fonts.gstatic.com/s/",
+            ],
+            "style-src": [
+              "'self'", "'unsafe-inline'",
+              "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/",
+              "https://fonts.googleapis.com/"
+            ]
+          },
+
+          permissionsPolicy: false
+        })
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig;
