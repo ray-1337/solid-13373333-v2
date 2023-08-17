1b5191c3ac6b4c825de40f49f0dfe2e11cbd13e9
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-08-17 08:52:12 +0200

    add mantine provider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,12 @@ import '../css/Reset.css';
 import '../css/Base.css';
 
 import { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
 
 export default function CDEVApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <MantineProvider>
+      <Component {...pageProps} />
+    </MantineProvider>
   );
 }
