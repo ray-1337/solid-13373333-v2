288ea2773478576dcbd2c656c4d41852e34f6cd4
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-08-17 07:54:02 +0200

    revamp projects

---
+++ src/components/project/detail.tsx
@@ -0,0 +1,58 @@
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { projectIndexContext, projectCurrentActive } from ".";
+import Projects from "./list";
+import mainStyle from "../../../../css/Main.module.css";
+import { marked } from "marked";
+import style from "./Project.Detail.module.css";
+
+export default function ProjectDetail() {
+  const { currentProjectIndex, setCurrentProjectIndex } = useContext(projectIndexContext);
+  const { currentProjectActive, setCurrentProjectActive } = useContext(projectCurrentActive);
+  const [currentProjectImageShow, setCurrentProjectImageShow] = useState<boolean>(false);
+
+  // https://13373333.one/?projectId=
+  const { query: params } = useRouter();
+  if (typeof params?.projectId === "string") {
+    const currentProjectID = params.projectId.toLowerCase();
+    const projectIndexFind = Projects.findIndex(val => val?.projectId === currentProjectID);
+
+    if (projectIndexFind >= 0) {
+      setCurrentProjectIndex(projectIndexFind);
+      setTimeout(() => setCurrentProjectActive(true), 125);
+    };
+  };
+
+  const currentProject = Projects[currentProjectIndex()!];
+
+  return (
+    <div className={style.project_details} data-show={currentProjectActive()}>
+      <div className={mainStyle.close} onClick={() => {
+        setCurrentProjectActive(false);
+        setParams({ projectId: null });
+        setTimeout(() => {
+          setCurrentProjectIndex(null);
+          setCurrentProjectImageShow(false);
+        }, 950);
+      }}>
+        <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" /></svg>
+      </div>
+
+      <div className={style.project_details_image} data-show={currentProjectImageShow()}>
+        <Image alt={""} layout={"fill"} src={currentProject.image} onLoad={() => setCurrentProjectImageShow(true)} />
+
+        <div className={style.project_details_image_title}>
+          <h1>{currentProject.title}</h1>
+          <p>{currentProject.description}</p>
+        </div>
+      </div>
+
+      <div className={style.project_details_html} dangerouslySetInnerHTML={{__html: marked.parseInline(currentProject?.html || currentProject.description || "Nothing to show here.", { gfm: true, breaks: true })}}></div>
+
+      <div className={style.project_details_checkout} style={{ display: currentProject?.resigned ? "none" : undefined }} onclick={() => window.open(currentProject.url, "_blank")}>
+        <h1>Visit</h1>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/project/index.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import { preventClick, sleep } from "../../Util";
+import Image from "next/image";
+
+import style from "./Project.module.css";
+import Projects from "./list";
+
+export default function(props: { active?: boolean }) {
+  const derivedProps = () => props.active;
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  // const [currentProjectIndex, setCurrentProjectIndex] = createSignal<number | null>(null);
+  // const [currentProjectActive, setCurrentProjectActive] = createSignal<boolean>(false);
+
+  const resignedType = (number: number) => {
+    switch (number) {
+      case 0: return null;
+      case 1: return "Discontinued";
+      case 2: return "Paused";
+      case 3: return "Hiatus";
+      default: return "";
+    };
+  };
+
+  useEffect(() => {
+    if (derivedProps()) {
+      sleep(125).then(() => setToggle(true));
+    } else {
+      sleep(750).then(() => setToggle(false));
+    };
+  });
+
+  return (
+    <div className={style.project} /*data-hidewhencurrent={currentProjectIndex() !== null}*/ data-itchi={String(toggle)} style={{
+      width: toggle ? "100%" : "0", height: toggle ? "auto" : 0
+    }}>
+      {
+        Projects.map((ctx, index) => {
+          return (
+            <div key={index} className={style.content} onClick={() => {
+              // if (currentProjectIndex() !== null) return;
+
+              // setCurrentProjectIndex(index);
+              // setTimeout(() => setCurrentProjectActive(true), 125);
+            }}>
+
+              <div className={style.prismatics}>
+                <Image alt={ctx.description} layout={"fill"} src={ctx.image} onLoad={(evt) => {
+                  evt.currentTarget.classList.add(style.jpuf);
+                  if (ctx?.resigned) evt.currentTarget.classList.add(style.archive);
+                }} draggable={false} loading={"lazy"} onContextMenu={(evt) => preventClick(evt)}/>
+              </div>
+
+              <div className={style.imagine}>
+                <p className={style.sike}>{ctx?.resigned ? `${ctx.title} (${resignedType(ctx?.resigned)})` : ctx.title}</p>
+              </div>
+
+              {
+                ctx?.description?.length >= 1 && (
+                  <div className={style.bleed}>
+                    <div className={style.cupcakke}>
+                      <p>{ctx.description}</p>
+                    </div>
+
+                    {
+                      ctx?.tools?.length && (
+                        <div className={style.kickback}>
+                          {
+                            ctx.tools.map((ico, icoIndex) => {
+                              return (
+                                <span key={icoIndex} className={style.brand}>
+                                  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill={`#${ico.hex != "000000" || ico.title == "Ableton Live" ? ico.hex : "FFFFFF"}`} d={ico.path} />
+                                  </svg>
+                                </span>
+                              );
+                            })
+                          }
+                        </div>
+                      )
+                    }
+                  </div>
+                )
+              }
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+};
