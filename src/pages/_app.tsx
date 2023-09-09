61f38d5fbe2f4219afcb1744175a42a89feb4e1e
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-09-09 04:47:48 +0200

    fix incorrect Head tags placement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,18 @@ import { MantineProvider } from '@mantine/core';
 
 export default function CDEVApp({ Component, pageProps }: AppProps) {
   const websiteTitle = "second hand embarrassment";
-  
+
   return (
-    <MantineProvider>
+    <>
       <Head>
         <title>{websiteTitle}</title>
 
         <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, shrink-to-fit=no, initial-scale=1.0" />
       </Head>
-
-      <Component {...pageProps} />
-    </MantineProvider>
+      
+      <MantineProvider>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   );
 }
