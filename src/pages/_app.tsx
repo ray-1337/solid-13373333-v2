a26df9e66b4d91dcba0252837b8c330d910b0d59
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-09-09 04:41:47 +0200

    move title tag to _app instead

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,13 @@ import { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 
 export default function CDEVApp({ Component, pageProps }: AppProps) {
+  const websiteTitle = "second hand embarrassment";
+  
   return (
     <MantineProvider>
       <Head>
+        <title>{websiteTitle}</title>
+
         <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, shrink-to-fit=no, initial-scale=1.0" />
       </Head>
 
