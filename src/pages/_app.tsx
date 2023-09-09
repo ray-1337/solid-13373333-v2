3a2db4ee5e681dbe45c83b04c2ae0b3b19f857d6
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-09-09 04:40:57 +0200

    add viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../css/Reset.css';
 import '../css/Base.css';
 
+import { Head } from 'next/document';
 import { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 
 export default function CDEVApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, shrink-to-fit=no, initial-scale=1.0" />
+      </Head>
+
       <Component {...pageProps} />
     </MantineProvider>
   );
