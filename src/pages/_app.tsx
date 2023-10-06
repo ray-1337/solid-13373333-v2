bd664d1421906e113c3330c57f842c3cdbd53b6f
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-06 08:23:25 +0200

    add preview.css in dev mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,8 @@ export default function CDEVApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        { process.env.NODE_ENV === "development" && (<link rel="preload stylesheet" href={"/preview.css"} as={"style"}></link>) }
+
         <title>{websiteTitle}</title>
 
         <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, shrink-to-fit=no, initial-scale=1.0" />
