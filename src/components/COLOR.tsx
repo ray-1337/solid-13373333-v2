dd42e9deae11760fabc0519d96812848efaea862
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-13 23:33:58 +0200

    [draft] set nonce from _app to color div

--- src/components/COLOR.tsx
+++ src/components/COLOR.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useContext } from "react";
 
 import styles from "../css/components/COLOR.module.css";
 
+import { NonceContext } from "@/pages/_app";
+
 export default function COLOR() {
+  const nonce = useContext(NonceContext);
+  
   const color: Record<number, string[]> = {
     0: ["#b92b27", "#c31432", "#FF416C", "#ff3131", "#FF4B2B", "#dd3e54", "#ED213A"],
     1: ["#8360c3", "#2ebf91", "#6be585", "#FDC830", "#d43799"],
@@ -28,7 +32,7 @@ export default function COLOR() {
   }, []);
 
   return (
-    <div className={`${styles.inside} ${isOfficiallyActive ? styles.aktif : ""}`}>
+    <div nonce={nonce} className={`${styles.inside} ${isOfficiallyActive ? styles.aktif : ""}`}>
       {
         shine.map((numb, index) => {
           return (
