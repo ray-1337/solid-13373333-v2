a57d609598b0630decd0420fbf87f6bc98d754af
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-13 23:01:23 +0200

    [draft] import google fonts to _app instead

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,6 +29,7 @@ const WebsiteApp = ({ Component, pageProps, nonce }: AppPostProps) => {
       <Head>
         { process.env.NODE_ENV === "development" && (<link rel="preload stylesheet" href={"/preview.css"} as={"style"} nonce={nonceRef.current}></link>) }
 
+        <link rel={"preload stylesheet"} as={"style"} nonce={nonceRef.current} href={"https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"}/>
 
         <title>{websiteTitle}</title>
 
