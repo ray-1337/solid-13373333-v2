cc8cf3fcf952262bb49ff9e443b5d14691f76414
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-13 23:14:11 +0200

    whitelist cdnURL in CSP

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { cdnURL } from "../config";
 
 export const config = {
   matcher: '/:path*',
@@ -7,9 +8,9 @@ export const config = {
 export function middleware(request: NextRequest) {
   const nonce = `nonce-${crypto.randomUUID()}`;
 
-  const scriptCSP = ['script-src',  "'self'", nonce];
+  const scriptCSP = ['script-src', "'self'", nonce, cdnURL];
 
-  const styleCSP = ['style-src', "'self'", "https://fonts.googleapis.com/css2", nonce];
+  const styleCSP = ['style-src', "'self'", "https://fonts.googleapis.com/css2", nonce, cdnURL];
 
   if (process.env.NODE_ENV === "development") {
     scriptCSP.push("'unsafe-eval'");
@@ -18,13 +19,13 @@ export function middleware(request: NextRequest) {
   };
 
   const csp = [
-    ['default-src', "'self'", nonce],
+    ['default-src', "'self'", nonce, cdnURL],
     scriptCSP, styleCSP,
     ['connect-src', "'self'", nonce],
-    ['img-src', "'self'", nonce],
+    ['img-src', "'self'", nonce, cdnURL],
     ['base-uri',  "'self'", nonce],
     ['form-action', "'self'", nonce],
-    ['font-src', "'self'", "https://fonts.gstatic.com/s/", nonce],
+    ['font-src', "'self'", "https://fonts.gstatic.com/s/", nonce, cdnURL],
     ['object-src', "'none'"],
     ['frame-ancestors', "'none'"],
     ['block-all-mixed-content'],
