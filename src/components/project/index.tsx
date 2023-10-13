f4d8d2237b0efb4808a7ea6df608a7d4c358dddf
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-14 01:38:16 +0200

    add a final version of projects filtering

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -1,14 +1,17 @@
+import { useDisclosure } from "@mantine/hooks";
 import { useEffect, useState } from "react";
 import { preventClick, sleep } from "../Utility";
 import ImageLoader from "../Loader";
 import Image from "next/image";
-
+import { Burger } from "@mantine/core";
 import style from "../../css/components/project/Project.module.css";
 import Projects from "./list";
 
 export default function(props: { active?: boolean }) {
   const derivedProps = () => props.active;
   const [toggle, setToggle] = useState<boolean>(false);
+  const [filter, setFilter] = useState<string[]>([]);
+  const [isFilterOpen, { open: openFilter, close: closeFilter }] = useDisclosure(false);
 
   // const [currentProjectIndex, setCurrentProjectIndex] = createSignal<number | null>(null);
   // const [currentProjectActive, setCurrentProjectActive] = createSignal<boolean>(false);
@@ -33,62 +36,114 @@ export default function(props: { active?: boolean }) {
   });
 
   return (
-    <div className={style.project} /*data-hidewhencurrent={currentProjectIndex() !== null}*/ data-itchi={String(toggle)} style={{
-      width: toggle ? "100%" : "0", height: toggle ? "auto" : 0
-    }}>
-      {
-        Projects.map((ctx, index) => {
-          return (
-            <div key={index} className={style.content} onClick={() => {
-              if (ctx?.resigned) return;
-              
-              window.open(ctx.url, "_blank");
-            }}>
+    <section>
+      {/* filter work */}
+      <section className={style.project_filter_work} data-state={isFilterOpen}>
+        <section className={style.project_filter_work_inner}>
+          {/* filtering tab */}
+          <div className={style.project_filter_work_classification}>
+            <h3>Filter</h3>
 
-              <div className={style.prismatics}>
-                <Image crossOrigin={"anonymous"} loader={ImageLoader} alt={ctx.description} fill={true} src={ctx.image} onLoad={(evt) => {
-                  evt.currentTarget.classList.add(style.jpuf);
-                  if (ctx?.resigned) evt.currentTarget.classList.add(style.archive);
-                }} draggable={false} loading={"lazy"} onContextMenu={(evt) => preventClick(evt)}/>
-              </div>
-
-              <div className={style.imagine}>
-                <p className={style.sike}>{ctx?.resigned ? `${ctx.title} (${resignedType(ctx?.resigned)})` : ctx.title}</p>
-                
-                {(!ctx?.resigned || ctx?.resigned <= 0) && (<p className={style.cite}>Visit</p>)}
-              </div>
-
+            <div className={style.project_filter_work_classification_list}>
               {
-                ctx?.description?.length >= 1 && (
-                  <div className={style.bleed}>
-                    <div className={style.cupcakke}>
-                      <p>{ctx.description}</p>
-                    </div>
+                Projects.map((val, index) => {
+                  return (
+                    <div className={style.project_filter_work_classification_individual} data-state={filter.includes(val.name)} key={index}
+                    onClick={() => setFilter((prev) => prev.includes(val.name) ? prev.filter(i => i !== val.name) : [...prev, val.name])}>
+                      <div className={style.project_filter_work_classification_individual_name}>
+                        <p>{val.name}</p>
+                      </div>
 
-                    {
-                      ctx?.tools?.length && (
-                        <div className={style.kickback}>
-                          {
-                            ctx.tools.map((ico, icoIndex) => {
-                              return (
-                                <span key={icoIndex} className={style.brand}>
-                                  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill={`#${ico.hex != "000000" || ico.title == "Ableton Live" || ico.title == "Express" ? ico.hex : "FFFFFF"}`} d={ico.path} />
-                                  </svg>
-                                </span>
-                              );
-                            })
-                          }
-                        </div>
-                      )
-                    }
-                  </div>
-                )
+                      <div className={style.project_filter_work_classification_individual_total}>
+                        <p>{val.projects.length}</p>
+                      </div>
+                    </div>
+                  )
+                })
               }
             </div>
-          );
-        })
-      }
-    </div>
+          </div>
+
+          {/* close button */}
+          <div className={style.project_filter_work_close} data-state={isFilterOpen} onClick={() => closeFilter()}>
+            <Burger size="xl" opened={true} color={"black"} className={style.project_filter_work_close_burger}/>
+          </div>
+        </section>
+      </section>
+
+      {/* project list */}
+      <div className={style.project} data-itchi={String(toggle)} style={{
+        width: toggle ? "100%" : 0,
+        height: toggle ? "100%" : 0,
+        overflowY: isFilterOpen ? "hidden" : "auto",
+        position: "absolute"
+      }}>
+        {
+          Projects.filter(val => filter?.length ? filter.includes(val.name) : true).map((rootProject) => {
+            const subProject = rootProject.projects;
+
+            return subProject.map((ctx, subProjectIndex) => {
+              return (
+                <div key={subProjectIndex} className={style.content} onClick={() => {
+                  if (ctx?.intermittentType) return;
+
+                  window.open(ctx.url, "_blank");
+                }}>
+
+                  <div className={style.prismatics}>
+                    <Image draggable={false} loading={"lazy"} onContextMenu={(evt) => preventClick(evt)} crossOrigin={"anonymous"} loader={ImageLoader} alt={ctx.description} fill={true} src={ctx.image} onLoad={(evt) => {
+                      evt.currentTarget.classList.add(style.jpuf);
+
+                      if (ctx?.intermittentType) {
+                        evt.currentTarget.classList.add(style.archive);
+                      };
+                    }} />
+                  </div>
+
+                  <div className={style.imagine}>
+                    <p className={style.sike}>{ctx?.intermittentType ? `${ctx.title} (${resignedType(ctx?.intermittentType)})` : ctx.title}</p>
+
+                    {(!ctx?.intermittentType || ctx?.intermittentType <= 0) && (<p className={style.cite}>Visit</p>)}
+                  </div>
+
+                  {
+                    ctx?.description?.length >= 1 && (
+                      <div className={style.bleed}>
+                        <div className={style.cupcakke}>
+                          <p>{ctx.description}</p>
+                        </div>
+
+                        {
+                          ctx?.tools?.length && (
+                            <div className={style.kickback}>
+                              {
+                                ctx.tools.map((ico, icoIndex) => {
+                                  return (
+                                    <span key={icoIndex} className={style.brand}>
+                                      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill={`#${ico.hex != "000000" || ico.title == "Ableton Live" || ico.title == "Express" ? ico.hex : "FFFFFF"}`} d={ico.path} />
+                                      </svg>
+                                    </span>
+                                  );
+                                })
+                              }
+                            </div>
+                          )
+                        }
+                      </div>
+                    )
+                  }
+                </div>
+              );
+            });
+          })
+        }
+      </div>
+
+      {/* filter work */}
+      <section className={style.project_filter_work_button} data-state={toggle && isFilterOpen} onClick={() => openFilter()}>
+        <h2>Filter Work</h2>
+      </section>
+    </section>
   );
 };
