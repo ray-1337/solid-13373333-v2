8c99c4b59f3a4a590e56c1afb3ee9b75d3c6ffa2
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-13 22:59:04 +0200

    remove next-safe from next.config.js (w/ experimental package imports)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-const nextSafe = require("next-safe");
-const { cdnDomain, cdnURL } = require("./config");
+const { cdnURL } = require("./config");
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -18,49 +17,10 @@ const nextConfig = {
   },
 
   transpilePackages: ['@tabler/icons-react'],
-  
-  modularizeImports: {
-    "@tabler/icons-react": {
-      transform: "@tabler/icons-react/dist/esm/icons/{{member}}",
-    },
 
-    "simple-icons": {
-      transform: "simple-icons/icons",
-      preventFullImport: true,
-      skipDefaultConversion: true
-    }
+  experimental: {
+    optimizePackageImports: ["simple-icons", "@tabler/icons-react"]
   },
-
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: nextSafe({
-          isDev: !isProd,
-
-          contentSecurityPolicy: {
-            "prefetch-src": false,
-            "img-src": ["'self'", cdnURL],
-            "default-src": ["'self'", cdnURL],
-            "script-src": ["'self'", "https://static.cloudflareinsights.com/beacon.min.js/", cdnURL],
-            "frame-src": ["'self'", cdnURL],
-            "connect-src": ["'self'", `wss://${cdnDomain}`, cdnURL],
-            "font-src": [
-              "'self'",
-              "https://fonts.gstatic.com/s/",
-            ],
-            "style-src": [
-              "'self'", "'unsafe-inline'",
-              "https://fonts.googleapis.com/",
-              cdnURL
-            ]
-          },
-
-          permissionsPolicy: false
-        })
-      },
-    ]
-  }
 }
 
 module.exports = nextConfig;
