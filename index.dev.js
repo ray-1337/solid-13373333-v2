6a7eebc03c5932da4498a9c2b1b1b9af12bce5f2
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-11-11 18:35:43 +0100

    explicitly set NODE_ENV to "development"

--- index.dev.js
+++ index.dev.js
@@ -2,6 +2,6 @@ require('dotenv/config');
 
 const { nextDev } = require("next/dist/cli/next-dev");
 
-process.env.NEXT_CPU_PROF = 1;
+process.env.NODE_ENV = "development";
 
 nextDev({ _: [], "--port": +process.env.DEV_PORT || 3000 });
