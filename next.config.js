2e8e127fe8f0c2a63aaeede4e743640fb168ca28
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-01-11 13:43:20 +0100

    deprecate cupcakke lyrics css classnames

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
-const { cdnURL, cupcakkeVaginaLyrics } = require("./config");
-const { relative } = require("node:path");
+const { cdnURL } = require("./config");
 
 const isProd = process.env.NODE_ENV === "production";
-
-let CACHE_CSS_INDEXES = 0;
-let CACHE_MAP = {};
-
-const wrappedLyrics = [...new Set(cupcakkeVaginaLyrics.toLowerCase().replace(/[^\w\d\s]/gim, "").split(/\s/gim).filter(x => x.length >= 1))];
-
-let lyricsInOrder = (context, _, localName) => {
-  const filepath = relative(context.rootContext, context.resourcePath);
-  const key = filepath + localName;
-
-  const cached = CACHE_MAP[key];
-  if (cached) return cached;
-
-  const result = wrappedLyrics[CACHE_CSS_INDEXES];
-
-  CACHE_CSS_INDEXES++;
-  CACHE_MAP[key] = result;
-
-  return result;
-};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -43,22 +22,6 @@ const nextConfig = {
 
   experimental: {
     optimizePackageImports: ["simple-icons", "@tabler/icons-react"]
-  },
-
-  webpack: (config) => {
-    let rules = config.module.rules
-      .find((rule) => typeof rule.oneOf === 'object')
-      .oneOf.filter((rule) => Array.isArray(rule.use));
-
-    rules.forEach((rule) => {
-      rule.use.forEach((moduleLoader) => {
-        if (moduleLoader.loader?.includes('css-loader') && !moduleLoader.loader?.includes('postcss-loader')) {
-          moduleLoader.options.modules.getLocalIdent = lyricsInOrder;
-        };
-      });
-    });
-    
-    return config;
   }
 }
 
